4d0b54c5a4a96b4087bb804171c4e0cc634be83a
Vitalii Inshakov <michael8913@gmail.com>
2018-03-27 22:26:43 +0300

    add features for start game

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,10 +4,8 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
-import Cannonball from './Cannonball';
 import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
-import Heart from './Heart';
 import StartGame from './StartGame';
 import Title from './Title';
 
@@ -31,20 +29,32 @@ const Canvas = (props) => {
       <Ground />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
-      <Cannonball position={{x: 0, y: -100}} />
       <CurrentScore score={15}/>
-      <FlyingObject position={{x: -150, y: -300}} />
-      <FlyingObject position={{x: 150, y: -300}} />
-      <Heart position={{x: -300, y: 35}} />
-      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
-      <Title />
+      { !props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+        </g>
+      }
+      { props.gameState.started && 
+        <g>
+          <FlyingObject position={{x: -150, y: -300}} />
+          <FlyingObject position={{x: 150, y: -300}} />
+        </g>
+      }
     </svg>
   )
 }
 
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
-  trackMouse: PropTypes.func.isRequired
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired
+  }).isRequired,
+  trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired
 }
 
 export default Canvas;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,24 @@
-import { MOVE_OBJECTS } from '../actions/index';
+import { MOVE_OBJECTS, START_GAME } from '../actions/index';
 import moveObjects from './moveObjects';
+import startGame from './startGame';
 
-const intialState = {
-  angle: 45
+const intialGameState = {
+  started: false,
+  kills: 0,
+  lives: 3
 };
+
+const initialState = {
+  angle: 45,
+  gameState: intialGameState
+}
 
 export default function reducer(state = intialState, action) {
   switch (action.type) {
     case MOVE_OBJECTS:
       return moveObjects(state, action);
+    case START_GAME:
+      return startGame(state, intialGameState);
     default:
       return state;
   }
